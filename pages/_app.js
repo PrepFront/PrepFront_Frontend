3339c59a1a6f9f7e4a924e59a3f2cedf1add4e44
Hiten Vats <hitenvats16@gmail.com>
2023-01-19 10:46:45 +0530

    removed app and doc directory from dashboard

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
-import '../styles/globals.css';
+import { NextUIProvider } from '@nextui-org/react';
 
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <NextUIProvider>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  );
 }
+
 export default App;
