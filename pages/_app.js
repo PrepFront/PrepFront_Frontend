47bcf8daa190a2eb85034cc4c572a637144af17b
Hiten Vats <hitenvats16@gmail.com>
2023-01-19 22:15:29 +0530

    fix css issues

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,9 @@
 import { NextUIProvider } from '@nextui-org/react';
 import "../styles/globals.css"
 
-function App({ Component, pageProps }) {
+function App({ Component, pageProps }){
   return (
-    <NextUIProvider>
       <Component {...pageProps} />
-    </NextUIProvider>
   );
 }
 
--- pages/_document.js
+++ pages/_document.js
@@ -1,27 +1,19 @@
 import React from 'react';
-import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { CssBaseline } from '@nextui-org/react';
+import { Html, Head, Main, NextScript } from 'next/document';
 
-class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return {
-      ...initialProps,
-      styles: React.Children.toArray([initialProps.styles])
-    };
-  }
-
-  render() {
-    return (
-      <Html lang="en">
-        <Head>{CssBaseline.flush()}</Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function () {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
 
-export default MyDocument;
