33808c7d6836e3310778c46cc72391992ed68567
itspkjha <86141517+itspkjha@users.noreply.github.com>
2023-02-09 22:05:44 +0530

    Added design for Login page

--- components/Newsletter.js
+++ components/Newsletter.js
@@ -1,17 +1,36 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import Lottie from "lottie-react";
 import newsletterLottie from "../assets/newsletterLottie.json";
 import TextField from '@mui/material/TextField';
-
-
-
+import validator from 'validator'
+import axios from "axios";
 
 export const Newsletter = () => {
+
+  const email = useRef();
+
   const [desktopsize, setDesktopSize] = useState(false);
+  const [isValid, setIsValid] = useState(true);
+  const [helpertext, setHelperText] = useState('');
+
+
+  function clickHandler() {
+    console.log(email.current.value)
+
+    const inputMail = email.current.value;
+
+    if (validator.isEmail(inputMail)) {
+      setIsValid(true);
+    } else {
+      setIsValid(false);
+      setHelperText('Enter a valid email address!');
+    }
+
+  }
+
 
 
   useEffect(() => {
-    console.log(window.innerWidth);
     if (window.innerWidth >= 1000) {
       setDesktopSize(true);
     }
@@ -23,12 +42,15 @@ export const Newsletter = () => {
       <div className="absolute m-[50px] p-[30px] text-center md:text-left md:p-[50px] xl:p-[200px] z-10">
         <p className="  text-s xs:text-regular md:text-2xl text-gray-600 font-medium ">Want to have an edge in your Interview Preparation?</p>
         <p className='font-semibold text-3xl sm:text-4xl md:text-4xl lg:text-6xl'><span className='text-secondaryColor'>Subscribe</span> to our <span className='text-primaryColor'>newsletter</span>!</p>
-        <div className='flex justify-center flex-col sm:flex-row items-center md:justify-start gap-x-4 pt-2'>
-          <TextField sx={{ width: '90%' }} id="outlined-basic" label="E-mail address" variant="outlined" />
-
+        <div className='flex justify-center  flex-col sm:flex-row items-center md:justify-start gap-x-4 pt-2'>
+          <TextField sx={{
+            width: '90%',
+            margin:'5px',
+          }} error={!isValid} helperText={helpertext} inputRef={email} id="outlined-basic" label="E-mail address" variant="outlined" size={desktopsize?"normal":"small"}/>
           <button
             type="button"
-            className=" m-2 w-[110px] h-[40px]  text-[15px] lg:w-[180px] lg:h-[55px] lg:text-[20px] flex justify-center items-center px-6 py-2.5 bg-secondaryColor text-white font-medium text-base rounded-lg shadow-md hover:bg-orange-600 hover:shadow-lg focus:bg-orange-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-yellow-700 active:shadow-lg transition duration-150 ease-in-out"
+            className="m-2 w-[110px] h-[40px]  text-[15px] lg:w-[180px] lg:h-[55px] lg:text-[20px] flex justify-center items-center px-6 py-2.5 bg-secondaryColor text-white font-medium text-base rounded-lg shadow-md hover:bg-orange-600 hover:shadow-lg focus:bg-orange-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-yellow-700 active:shadow-lg transition duration-150 ease-in-out"
+            onClick={clickHandler}
           >
             Subscribe
           </button>
---
+++ pages/login.js
@@ -0,0 +1,164 @@
+import * as React from 'react';
+import { useState, useRef } from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import API_CLIENT from '../utils/api'
+import loginImg from '../assets/login.svg';
+import Image from 'next/image';
+import LINK from "../constants/Backend_links"
+
+
+function Copyright(props) {
+    return (
+        <Typography variant="body2" color="text.secondary" align="center" {...props}>
+            {'Copyright © '}
+            <Link color="inherit" href="">
+                PrepFront
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
+const theme = createTheme();
+
+export default function login() {
+
+    const [msg,setMsg] =useState('');
+    const [email,setEmail] = useState('');
+    const [pwd,setPwd] = useState('');
+    
+    const getEmailData = (e)=>{
+        setEmail(e.target.value);
+    }
+
+    const getPwdData = (e)=>{
+        setPwd(e.target.value);
+    }
+
+
+    const handleSubmit = async (event) => {
+
+        event.preventDefault();
+
+        const uid = email;
+        const password= pwd;
+
+        console.log({email:uid , password:password})
+
+        try{
+            const res = await API_CLIENT.post(LINK.ROUTES.USER.LOGIN,{email:uid , password:password});
+            console.log(res.data);
+
+        }
+        catch(e){
+            console.log('Error in Login Call ', e.message);
+        }
+
+        
+
+
+    };
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Grid container component="main" sx={{ height: '100vh' }}>
+                <CssBaseline />
+                <Grid
+                    item
+                    xs={false}
+                    sm={4}
+                    md={7}
+                    sx={{
+                        backgroundRepeat: 'no-repeat',
+                        backgroundColor: (t) =>
+                            t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                        display:'flex',
+                        justifyContent:'center',
+                    }}
+                >
+                    <Image  src={loginImg} width={800} height={800}></Image>
+                </Grid>
+                <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+                    <Box
+                        sx={{
+                            my: 8,
+                            mx: 4,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Avatar sx={{ m: 1, bgcolor: '#686FC8' }}>
+                            <LockOutlinedIcon />
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                            Sign in
+                        </Typography>
+                        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                                autoFocus
+                                onChange={getEmailData}
+                            />
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                                onChange={getPwdData}
+                            />
+                            {/* <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              /> */}
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="outlined"
+                                sx={{ mt: 3, mb: 2}}
+                            >
+                                Sign In
+                            </Button>
+                            <Grid container>
+                                <Grid item xs>
+                                    <Link href="#" variant="body2">
+                                        Forgot password?
+                                    </Link>
+                                </Grid>
+                                <Grid item>
+                                    <Link href="#" variant="body2">
+                                        {"Don't have an account? Sign Up"}
+                                    </Link>
+                                </Grid>
+                            </Grid>
+                            <Copyright sx={{ mt: 5 }} />
+                        </Box>
+                    </Box>
+                </Grid>
+            </Grid>
+        </ThemeProvider>
+    );
+}
